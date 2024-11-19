829e179f65399bc40cb5301c6dfefd68a4e1a862
yooncheol <oyc1230@gmail.com>
2024-11-19 15:31:04 +0900

    Dto Print Test

--- src/main/java/org/kr/co/sports_result/soccer/Service/PremierService.java
+++ src/main/java/org/kr/co/sports_result/soccer/Service/PremierService.java
@@ -1,22 +1,37 @@
 package org.kr.co.sports_result.soccer.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kr.co.sports_result.common.exception.PremierLeagueDataNotFoundException;
+import org.kr.co.sports_result.common.util.DateUtil;
 import org.kr.co.sports_result.soccer.Dto.PremierResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PremierService {
 
-    private static final String API_URL = "https://site.web.api.espn.com/apis/site/v2/sports/soccer/scorepanel?league=ENG.1&region=kr&lang=kr&contentorigin=espn&limit=250&dates=20241110";
-
     public void fetchEplResults() {
+        String apiUrl = "https://site.web.api.espn.com/apis/site/v2/sports/soccer/scorepanel?league=ENG.1&region=kr&lang=kr&contentorigin=espn&limit=250&dates=" + DateUtil.getCurrentDate();
+
         RestTemplate restTemplate = new RestTemplate();
 
         try {
-            System.out.println(restTemplate.getForObject(API_URL, PremierResult.class));
+            PremierResult premierResult = restTemplate.getForObject(apiUrl, PremierResult.class);
 
+            if(premierResult.getScores().get(0).getEvents().isEmpty()) {
+                log.info(DateUtil.getToday() + " 프리미어 리그 경기가 없습니다.");
+                throw new PremierLeagueDataNotFoundException(DateUtil.getToday() + " 프리미어 리그 경기가 없습니다.");
+            }
+
+            premierResult.getScores().get(0).getEvents().forEach(event -> {
+               System.out.println(event.getCompetitions().toString());
+               event.getCompetitions().forEach(competition -> {
+                   System.out.println(DateUtil.getKoreaDate(competition.getStartDate()));
+               });
+            });
 
         }catch (Exception e){
             e.printStackTrace();
